918c0c5ee3a157ae45838884fd9462a9a0eb0512
Honey the Bear <53951776+HoneyTheYellowBear123@users.noreply.github.com>
2022-02-16 21:37:06 -0500

    more solstice work

--- Heath Leader/Core/HeathSolsticeDistrict.sql
+++ Heath Leader/Core/HeathSolsticeDistrict.sql
@@ -88,6 +88,16 @@
 		INSERT INTO DistrictReplaces
 					(CivUniqueDistrictType,					ReplacesDistrictType)
 		VALUES		('DISTRICT_HEATH_SOLSTICE',				'DISTRICT_HOLY_SITE');
+
+
+	-----------------------------------------------------------------------------------
+	-- DistrictAdjacencies
+	---------------------------------------------------------------------------------------
+		INSERT INTO District_Adjacencies
+					(DistrictType,							YieldChangeID)
+		SELECT 		'DISTRICT_HEATH_SOLSTICE',			    YieldChangeID
+
+		FROM District_Adjacencies WHERE DistrictType = 'DISTRICT_HOLY_SITE';
 	-----------------------------------------------------------------------------------
 	-- District_ValidTerrains
 	-----------------------------------------------------------------------------------
@@ -103,11 +113,11 @@
 					('DISTRICT_HEATH_SOLSTICE',					'TERRAIN_SNOW_HILLS');
 
 	-------------------------------------------------------------------------------------
-	-- Great Prophet Points
+	-- Great Person Points
 	--------------------------------------------------------------------------------------
 		INSERT INTO District_GreatPersonPoints
 					(DistrictType,							GreatPersonClassType,           PointsPerTurn)
-		VALUES      
+		VALUES      ('DISTRICT_HEATH_SOLSTICE',             'GREAT_PERSON_CLASS_PROPHET',   '1'          )              
 
 
 	-----------------------------------------------------------------------------------
@@ -123,94 +133,6 @@
 		INSERT INTO MomentIllustrations
 				(MomentIllustrationType, 						MomentDataType,					GameDataType,					Texture)
 		VALUES	('MOMENT_ILLUSTRATION_UNIQUE_DISTRICT', 		'MOMENT_DATA_DISTRICT',			'DISTRICT_HEATH_SOLSTICE',		'Moment_Infrastructure_Heath_Solstice.dds');
---==========================================================================================================================
--- Food and Production
---==========================================================================================================================
-	-- District_Adjacencies
-	-----------------------------------------------------------------------------------
-		INSERT INTO District_Adjacencies
-				(DistrictType,					YieldChangeId)
-		VALUES	('DISTRICT_SUK_TORFBAEIR',		'Torfbaeir_Base_Food'),
-				('DISTRICT_SUK_TORFBAEIR',		'Torfbaeir_Base_Production');
-	-----------------------------------------------------------------------------------
-	-- Adjacency_YieldChanges
-	-----------------------------------------------------------------------------------
-		INSERT OR REPLACE INTO Adjacency_YieldChanges
-			(
-				ID,
-				Description,
 
-				YieldType,
-				YieldChange,
+	-------------------------------------------------------------------
 
-				Self
-			)
-		VALUES
-			(
-				'Torfbaeir_Base_Food',
-				'LOC_DISTRICT_SUK_TORFBAEIR_BASE_FOOD',
-
-				'YIELD_FOOD',
-				1,
-
-				1
-			),
-			(
-				'Torfbaeir_Base_Production',
-				'LOC_DISTRICT_SUK_TORFBAEIR_BASE_PRODUCTION',
-
-				'YIELD_PRODUCTION',
-				1,
-
-				1
-			);
-	-------------------------------------
-	-- DistrictModifiers/TraitModifiers
-	-------------------------------------
-		INSERT INTO TraitModifiers
-				(TraitType,												ModifierId)
-		VALUES	('TRAIT_CIVILIZATION_DISTRICT_SUK_TORFBAEIR', 			'DISTRICT_SUK_TORFBAEIR_CHARMING_FOOD'),
-				('TRAIT_CIVILIZATION_DISTRICT_SUK_TORFBAEIR', 			'DISTRICT_SUK_TORFBAEIR_CHARMING_PROD'),
-
-				('TRAIT_CIVILIZATION_DISTRICT_SUK_TORFBAEIR', 			'DISTRICT_SUK_TORFBAEIR_BREATHTAKING_FOOD'),
-				('TRAIT_CIVILIZATION_DISTRICT_SUK_TORFBAEIR', 			'DISTRICT_SUK_TORFBAEIR_BREATHTAKING_PROD');
-	-------------------------------------
-	-- Modifiers
-	-------------------------------------
-		INSERT INTO Modifiers
-				(ModifierId,											ModifierType,															SubjectRequirementSetId)
-		VALUES	('DISTRICT_SUK_TORFBAEIR_CHARMING_FOOD',				'MODIFIER_PLAYER_DISTRICTS_ADJUST_YIELD_BASED_ON_APPEAL',				NULL),
-				('DISTRICT_SUK_TORFBAEIR_CHARMING_PROD',				'MODIFIER_PLAYER_DISTRICTS_ADJUST_YIELD_BASED_ON_APPEAL',				NULL),
-
-				('DISTRICT_SUK_TORFBAEIR_BREATHTAKING_FOOD',			'MODIFIER_PLAYER_DISTRICTS_ADJUST_YIELD_BASED_ON_APPEAL',				NULL),
-				('DISTRICT_SUK_TORFBAEIR_BREATHTAKING_PROD',			'MODIFIER_PLAYER_DISTRICTS_ADJUST_YIELD_BASED_ON_APPEAL',				NULL);
-	-------------------------------------
-	-- ModifierArguments
-	-------------------------------------
-		INSERT INTO ModifierArguments
-				(ModifierId,										Name,							Value)
-		VALUES	('DISTRICT_SUK_TORFBAEIR_CHARMING_FOOD',			'YieldType',					'YIELD_FOOD'),
-				('DISTRICT_SUK_TORFBAEIR_CHARMING_FOOD',			'YieldChange',					1),
-				('DISTRICT_SUK_TORFBAEIR_CHARMING_FOOD',			'Description',					'LOC_DISTRICT_SUK_TORFBAEIR_APPEAL_FOOD'),
-				('DISTRICT_SUK_TORFBAEIR_CHARMING_FOOD',			'DistrictType',					'DISTRICT_SUK_TORFBAEIR'),
-				('DISTRICT_SUK_TORFBAEIR_CHARMING_FOOD',			'RequiredAppeal',				2),
-
-				('DISTRICT_SUK_TORFBAEIR_CHARMING_PROD',			'YieldType',					'YIELD_PRODUCTION'),
-				('DISTRICT_SUK_TORFBAEIR_CHARMING_PROD',			'YieldChange',					1),
-				('DISTRICT_SUK_TORFBAEIR_CHARMING_PROD',			'Description',					'LOC_DISTRICT_APPEAL_PRODUCTION'),
-				('DISTRICT_SUK_TORFBAEIR_CHARMING_PROD',			'DistrictType',					'DISTRICT_SUK_TORFBAEIR'),
-				('DISTRICT_SUK_TORFBAEIR_CHARMING_PROD',			'RequiredAppeal',				2),
-
-				('DISTRICT_SUK_TORFBAEIR_BREATHTAKING_FOOD',		'YieldType',					'YIELD_FOOD'),
-				('DISTRICT_SUK_TORFBAEIR_BREATHTAKING_FOOD',		'YieldChange',					1),
-				('DISTRICT_SUK_TORFBAEIR_BREATHTAKING_FOOD',		'Description',					'LOC_DISTRICT_SUK_TORFBAEIR_APPEAL_FOOD'),
-				('DISTRICT_SUK_TORFBAEIR_BREATHTAKING_FOOD',		'DistrictType',					'DISTRICT_SUK_TORFBAEIR'),
-				('DISTRICT_SUK_TORFBAEIR_BREATHTAKING_FOOD',		'RequiredAppeal',				4),
-
-				('DISTRICT_SUK_TORFBAEIR_BREATHTAKING_PROD',		'YieldType',					'YIELD_PRODUCTION'),
-				('DISTRICT_SUK_TORFBAEIR_BREATHTAKING_PROD',		'YieldChange',					1),
-				('DISTRICT_SUK_TORFBAEIR_BREATHTAKING_PROD',		'Description',					'LOC_DISTRICT_APPEAL_PRODUCTION'),
-				('DISTRICT_SUK_TORFBAEIR_BREATHTAKING_PROD',		'DistrictType',					'DISTRICT_SUK_TORFBAEIR'),
-				('DISTRICT_SUK_TORFBAEIR_BREATHTAKING_PROD',		'RequiredAppeal',				4);
---==========================================================================================================================
---==========================================================================================================================
